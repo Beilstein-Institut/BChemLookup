5f0f0fa80674719179062b23f4d5e4c454dda864
Felix Bänsch <168176402+fbaensch-beilstein@users.noreply.github.com>
2025-08-13 08:56:15 +0200

    Fix bash script;

--- scripts/check_duplicates_abbreviations.sh
+++ scripts/check_duplicates_abbreviations.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-if [! -f abbreviations.smi]; then
-  echo "abbreviations.smi file not found!"
+if [ ! -f abbreviations.smi ]; then
+  echo "abbreviatio.smi file not found!"
   exit 1
 fi  
 
 duplicates=$(sort abbreviations.smi | uniq -d)
-if [-n "$duplicates"]; then
+if [ -n "$duplicates" ]; then
   echo "Duplicate entries found in abbreviations.smi:"
   echo "$duplicates"
   exit 1
