4c85dedc6660d7dea1243a1c97ed3e6f7c1b2db0
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-11-05 15:40:59 +0100

    Update lint.py

--- lint.py
+++ lint.py
@@ -13,25 +13,11 @@ HERE = Path(__file__).parent.resolve()
 
 
 def main():
-    tsv_path = HERE.joinpath("abbreviations.csv")
-    df1 = pd.read_csv(tsv_path)
-
     smi_path = HERE.joinpath("abbreviations.smi")
     df2 = pd.read_csv(smi_path, sep="\t")
 
-    if not (df1.columns == df2.columns).all():
-        raise
-
-    s1 = set(df1['Abbreviation'])
-    s2 = set(df2['Abbreviation'])
-
-    d1 = s1 - s2
-    if d1:
-        raise ValueError(f"abbreviations in CSV but not SMI: {d1}")
-
-    d2 = s2 - s1
-    if d2:
-        raise ValueError(f"abbreviations in SMI but not CSV: {d2}")
+    if smi_path.read_text() != df2.to_csv(sep='\t', index=False):
+        raise ValueError(f"{smi_path} is not formatted properly.")
 
 
 if __name__ == '__main__':
